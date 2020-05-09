c8a51af3f12edff98d2c023a91992f2ed35fedc4
George Tsiga <tsiga.george@gmail.com>
2020-05-09 16:02:32 +0200

    Added xml app

---
+++ conference -xml/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,18 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<Speaker>();
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("George");
+        speaker.setLastName("Tsiga");
+        speakers.add(speaker);
+        return speakers;
+    }
+}
