4643f553c2726d23617c4ad9b5b13de76ebb8d77
George Tsiga <tsiga.george@gmail.com>
2020-05-26 22:16:39 +0200

    Tymeleaf implementation

--- conference-mvc/src/main/java/com/pluralsight/conferencedemo/ConferenceConfig.java
+++ conference-mvc/src/main/java/com/pluralsight/conferencedemo/ConferenceConfig.java
@@ -1,19 +1,35 @@
 package com.pluralsight.conferencedemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     private String WEB_INF_PATH = "/WEB-INF/";
     private String JSP_PATH = WEB_INF_PATH + "jsp/";
+    private String VIEWS_PATH = WEB_INF_PATH + "views/";
     private String PDF_RESOURCE_PATH = WEB_INF_PATH + "pdf/";
-    private String SUFFIX = ".jsp";
+    private String SUFFIX_JSP = ".jsp";
+    private String SUFFIX_HTML = ".html";
     private String FILES_WILD_CARD = "/files/**";
 
 
@@ -23,13 +39,58 @@ public class ConferenceConfig implements WebMvcConfigurer {
                 .addResourceLocations(PDF_RESOURCE_PATH);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.getDefault());
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
         bean.setPrefix(JSP_PATH);
-        bean.setSuffix(SUFFIX);
-        bean.setOrder(0);
+        bean.setSuffix(SUFFIX_JSP);
+        bean.setOrder(1);
         return bean;
     }
+
+    @Bean
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(0);
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix(VIEWS_PATH);
+        templateResolver.setSuffix(SUFFIX_HTML);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
 }
 
--- conference-mvc/src/main/java/com/pluralsight/conferencedemo/controllers/GreetingController.java
+++ conference-mvc/src/main/java/com/pluralsight/conferencedemo/controllers/GreetingController.java
@@ -7,9 +7,16 @@ import java.util.Map;
 
 @Controller
 public class GreetingController {
+
     @GetMapping("greeting")
     public String greeting(Map<String, Object> model) {
         model.put("message", "Hello George");
         return "greeting";
+    }
+
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model) {
+        model.put("message", "Hello Thymeleaf");
+        return "thyme";
     }
 }
